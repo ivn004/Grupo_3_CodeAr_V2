81745f8adde01be33cdb9e4ece9cd057be95edce
ivn004 <ivnbeinitez@gmail.com>
2021-04-08 20:29:43 -0300

    trabajando con base de datos

--- Codear/controllers/productController.js
+++ Codear/controllers/productController.js
@@ -1,104 +1,87 @@
-/* const db = require ('../../database/models') */
+const db = require("../database/models");
 const path = require("path");
-const { getProducts, setProducts } = require(path.join("..", "data", "products")); // Requiere la ruta del JSON parseado.
+const { getProducts, setProducts } = require(path.join(
+  "..",
+  "data",
+  "products"
+)); // Requiere la ruta del JSON parseado.
 const products = getProducts();
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."); // Formula para los puntos de los miles.
 
-
 const productController = {
-    //DETALLE
-    detail: (req, res) => {
-        const { id } = req.params
+  //DETALLE
+  detail: (req, res) => {
+    const { id } = req.params;
 
-        let resultado = products.find(product => {
-            return product.id === +id
-        })
+    let resultado = products.find((product) => {
+      return product.id === +id;
+    });
 
-        res.render("productDetail", {
-            title: "detalle",
-            resultado,
-            products,
-            id
-        })
-    },
+    res.render("productDetail", {
+      title: "detalle",
+      resultado,
+      products,
+      id,
+    });
+  },
 
-    //BUSCADOR
-    search: (req, res) => {
-        const buscar = req.query.search
+  //BUSCADOR
+  search: (req, res) => {
+    const buscar = req.query.search;
 
-        const resultado = products.filter(product => {
-            return (product.nombreCurso.toLowerCase()).includes(buscar.toLowerCase())
+    const resultado = products.filter((product) => {
+      return product.nombreCurso.toLowerCase().includes(buscar.toLowerCase());
+    });
+
+    res.render("resultados", {
+      title: "Resultado de la búsqueda",
+      products: resultado,
+    });
+  },
+
+  //--------------CARRITO-----------------//
+  //--------------------------------------//
+  //--------------CARRITO-----------------//
+
+  carrito: (req, res) => {
+    db.Product.findAll().then(function (products) {
+      res.render("carritoViews/productCar", {
+        title: "Cursos",
+        products,
+        toThousand,
+      });
+    });
+    // Renderiza en la vista "ProductCar" y crea los nuevos arrays y metodos para usar.
+  },
+
+  categorias: (req, res) => {
+    const { id } = req.params;
+    db.Product.findAll({
+      where: {
+        categoryId: id,
+      },
+    }).then(function (products) {
+      db.Category.findByPk(id).then((categoria) => {
+        res.render("carritoViews/productCar", {
+          title: categoria.nombre,
+          products,
+          toThousand,
         });
+      });
+    });
+    // Renderiza en la vista "ProductCar" y crea los nuevos arrays y metodos para usar.
+  },
 
-        res.render("resultados", {
-            title: "Resultado de la búsqueda",
-            products: resultado
-        })
-    },
+  packCursos: (req, res) => {
+    const productoFiltra = products.filter((producto) => {
+      return producto.categoria === "pack";
+    });
 
-
-    //--------------CARRITO-----------------//
-    //--------------------------------------//
-    //--------------CARRITO-----------------//
-
-
-    carrito: (req, res) => {
-        res.render('carritoViews/productCar', {
-            title: "Cursos",
-            products,
-            toThousand
-        }); // Renderiza en la vista "ProductCar" y crea los nuevos arrays y metodos para usar.
-    },
-    packCursos:(req,res)=>{
-        const productoFiltra = products.filter(producto => {
-            return producto.categoria === 'pack'
-        })
-
-        res.render("packs",{
-            title:"Packs",
-            productoFiltra
-        })
-    },
-
-
-    //--------------CATEGORIAS-CARRITO-----------------//
-    //-------------------------------------------------//
-    //--------------CATEGORIAS-CARRITO-----------------//
-    diseñoWeb:(req,res)=>{
-
-        const productoFiltra = products.filter(producto => {
-            return producto.categoria === 'diseñoWeb'
-        })
-        res.render('carritoViews/diseñoweb', {
-            title: "Diseño Web",
-            productoFiltra,
-        });
-        
-    },
-    js: (req, res) => {
-
-        const productoFiltra = products.filter(producto => {
-            return producto.categoria === 'JavaScript'
-        })
-
-        res.render('carritoViews/js', {
-            title: "JavaScript",
-            productoFiltra,
-        });
-
-    },
-    videoJuegos:(req,res) =>{
-
-        const productoFiltra = products.filter(producto => {
-            return producto.categoria === 'Video Juegos'
-        })
-        res.render('carritoViews/videoJuegos', {
-            title: "Video Juegos",
-            productoFiltra,
-        });
-        
-    } 
-
+    res.render("packs", {
+      title: "Packs",
+      productoFiltra,
+    });
+  },
 };
 
 module.exports = productController;
--- Codear/routes/productRouter.js
+++ Codear/routes/productRouter.js
@@ -3,7 +3,7 @@ var router = express.Router(); // Permite crear múltiples "mini aplicaciones".
 const path = require("path");
 
 // Rutas a los controladores.
-const {detail, carrito, search, diseñoWeb, js, videoJuegos} = require(path.join("..","controllers","productController"));
+const {detail, carrito, search,categorias} = require(path.join("..","controllers","productController"));
 
 //SEARCH
 router.get("/search",search)
@@ -14,8 +14,8 @@ router.get('/detail/:id', detail)
 /* GET Página CARRITO */
 router.get("/carrito",carrito)
 //CATEGORIAS
-router.get("/webdesing", diseñoWeb)
-router.get("/js", js)
-router.get("/videoJuegos", videoJuegos)
+
+router.get('/categoria/:id',categorias)
+
 
 module.exports = router;
